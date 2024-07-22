7a88f1bbe28c96e7f8ed50c37b9582d957e9ae63
BoundlessFate <villac3@rpi.edu>
2024-07-22 16:52:25 -0400

    finished im pretty sure

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ class rand48:
 def next_exp():
     num = -math.log(rand.drand48())/lmda
     if num > upBound:
-        return upBound
+        return next_exp()
     return num
 
 # USE THIS CODE BELOW ONLY FOR TESTING!!! WILL REDIRECT STDERR
@@ -40,22 +40,44 @@ nCpu = int(sys.argv[2])
 seed = int(sys.argv[3])
 lmda = float(sys.argv[4])
 upBound = int(sys.argv[5])
+if (n <= 0 or nCpu <= 0 or seed <= 0 or lmda <= 0 or upBound <= 0):
+    sys.stderr.write("ERROR: wrong number of inputs given")
+    os.abort()
 rand = rand48()
 rand.srand48(seed)
 arrivalTimes = []
 CPUBurstTimes = []
 IOBurstTimes = []
 numCPUBurst = []
+# total time spent doing io/cpu bursts for io/cpu bound processes
 cpuTotalCpu = 0
 ioTotalCpu = 0
 cpuTotalIO = 0
 ioTotalIO = 0
+# Total number of bursts for io/cpu for io/cpu bound processes
+totalNumCPUBurstsFromCPU = 0
+totalNumIOBurstsFromCPU = 0
+totalNumCPUBurstsFromIO = 0
+totalNumIOBurstsFromIO = 0
+#pseudo random numbers and predictability
+# for each process
 for i in range(0,n):
+    #1
     initProcessArrivalTime = math.floor(next_exp())
+    #2
     numCPUBursts = math.ceil(rand.drand48() * 32)
     numCPUBurst.append(numCPUBursts)
+    #3
     cburstTimes = []
     iburstTimes = []
+    # Add total num of bursts
+    if i < nCpu:
+        totalNumCPUBurstsFromCPU += numCPUBursts
+        totalNumIOBurstsFromCPU += numCPUBursts - 1
+    else:
+        totalNumCPUBurstsFromIO += numCPUBursts
+        totalNumIOBurstsFromIO += numCPUBursts - 1
+
     for j in range(0,numCPUBursts):
         CPUBurstTime = math.ceil(next_exp())
         IOBurstTime = 0
@@ -80,7 +102,7 @@ for i in range(0,n):
 letters = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J",10:"K",11:"L",12:"M",13:"N",14:"O",15:"P",16:"Q",17:"R",18:"S",19:"T",20:"U",21:"V",22:"W",23:"X",24:"Y",25:"Z"}
 
 # Final outputs to terminal after everything is calculated
-print("<<< Project PART I")
+print("<<< PROJECT PART I")
 if (nCpu == 1):
     print(f"<<< -- process set (n={n}) with 1 CPU-bound process")
 else:
@@ -89,9 +111,15 @@ print(f"<<< -- seed={seed}; lambda={lmda:.6f}; bound={upBound}")
 for i in range(0,len(arrivalTimes)):
     id = letters[math.floor(i/10)]+str(i%10)
     if i < nCpu:
-        print(f"CPU-bound process {id}: arrival time {arrivalTimes[i]}ms; {numCPUBurst[i]} CPU bursts:")
+        if numCPUBurst[i] == 1:
+            print(f"CPU-bound process {id}: arrival time {arrivalTimes[i]}ms; 1 CPU burst:")
+        else:
+            print(f"CPU-bound process {id}: arrival time {arrivalTimes[i]}ms; {numCPUBurst[i]} CPU bursts:")
     else:
-        print(f"I/O-bound process {id}: arrival time {arrivalTimes[i]}ms; {numCPUBurst[i]} CPU bursts:")
+        if numCPUBurst[i] == 1:
+            print(f"I/O-bound process {id}: arrival time {arrivalTimes[i]}ms; 1 CPU burst:")
+        else:
+            print(f"I/O-bound process {id}: arrival time {arrivalTimes[i]}ms; {numCPUBurst[i]} CPU bursts:")
     for j in range (0, len(CPUBurstTimes[i])):
         curStr = "==> CPU burst " + str(CPUBurstTimes[i][j]) + "ms"
         if (IOBurstTimes[i][j] != 0):
@@ -101,11 +129,20 @@ f = open("simout.txt", "w")
 f.write(f"-- number of processes: {n}\n")
 f.write(f"-- number of CPU-bound processes: {nCpu}\n")
 f.write(f"-- number of I/O-bound processes: {n-nCpu}\n")
-f.write(f"-- CPU-bound average CPU burst time: {math.ceil(cpuTotalCpu/nCpu*1000)/1000:.3f}ms\n")
-f.write(f"-- I/O-bound average CPU burst time: {math.ceil(ioTotalCpu/(n-nCpu)*1000)/1000:.3f}ms\n")
-f.write(f"-- overall average CPU burst time: {math.ceil((cpuTotalCpu+ioTotalCpu)/n*1000)/1000:.3f}ms\n")
-f.write(f"-- CPU-bound average I/O burst time: {math.ceil(cpuTotalIO/nCpu*1000)/1000:.3f}ms\n")
-f.write(f"-- I/O-bound average I/O burst time: {math.ceil(ioTotalIO/(n-nCpu)*1000)/1000:.3f}ms\n")
-f.write(f"-- overall average I/O burst time: {math.ceil((cpuTotalIO+ioTotalIO)/n*1000)/1000:.3f}ms")
+# Dont need to check these because num cpu bursts is ceiling
+f.write(f"-- CPU-bound average CPU burst time: {math.ceil(cpuTotalCpu/totalNumCPUBurstsFromCPU*1000)/1000:.3f} ms\n")
+f.write(f"-- I/O-bound average CPU burst time: {math.ceil(ioTotalCpu/totalNumCPUBurstsFromIO*1000)/1000:.3f} ms\n")
+f.write(f"-- overall average CPU burst time: {math.ceil((cpuTotalCpu+ioTotalCpu)/(totalNumCPUBurstsFromCPU+totalNumCPUBurstsFromIO)*1000)/1000:.3f} ms\n")
+# Need to check these because it is num cpu bursts - 1 so can be 0
+if totalNumIOBurstsFromCPU == 0:
+    f.write(f"-- CPU-bound average I/O burst time: {0:.3f} ms\n")
+else:
+    f.write(f"-- CPU-bound average I/O burst time: {math.ceil(cpuTotalIO/totalNumIOBurstsFromCPU*1000)/1000:.3f} ms\n")
+if totalNumIOBurstsFromIO == 0:
+    f.write(f"-- I/O-bound average I/O burst time: {0:.3f} ms\n")
+else:
+    f.write(f"-- I/O-bound average I/O burst time: {math.ceil(ioTotalIO/totalNumIOBurstsFromIO*1000)/1000:.3f} ms\n")
+# Dont need to check because works with ceiling function to be at min 1
+f.write(f"-- overall average I/O burst time: {math.ceil((cpuTotalIO+ioTotalIO)/(totalNumIOBurstsFromCPU+totalNumIOBurstsFromIO)*1000)/1000:.3f} ms")
 f.close()
 sys.stdout.close()
